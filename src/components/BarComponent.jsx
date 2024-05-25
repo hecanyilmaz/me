f17b014770403db5205560f69d527add8235900b
hecanyilmaz <hcanylmzbotpre@gmail.com>
2024-05-25 14:17:51 +0300

    v1

--- src/components/BarComponent.jsx
+++ src/components/BarComponent.jsx
@@ -1,18 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function BarComponent() {
     return (
         <div className="bar-component">
             <ul role="menu-bar">
                 <li role="menu-item" tabindex="0" aria-haspopup="false">
-                    <a href="/">Home</a>
+                    <a><Link to={"/"} className="mylink">Home</Link></a>
                 </li>
                 <li role="menu-item" tabindex="0" aria-haspopup="true">
                     Explore
                     <ul role="menu">
-                        <li role="menu-item"><a href="/soon">Technologies</a></li>
-                        <li role="menu-item"><a href="/soon">Projects</a></li>
-                        <li role="menu-item"><a href="/soon">Hobbies</a></li>
-                        <li role="menu-item"><a href="/contact">Contact</a></li>
-                        {/* ADD "style={{textDecoration: 'none'}}" INTO THE ITEMS */}
+                        <li role="menu-item"><a><Link to={"/soon"} className="mylink">Technologies</Link></a></li>
+                        <li role="menu-item"><a><Link to={"/soon"} className="mylink">Projects</Link></a></li>
+                        <li role="menu-item"><a><Link to={"/soon"} className="mylink">Hobbies</Link></a></li>
+                        <li role="menu-item"><a><Link to={"/contact"} className="mylink">Contact</Link></a></li>
                     </ul>
                 </li>
                 <li role="menu-item" tabindex="0" aria-haspopup="true">
@@ -25,15 +26,17 @@ export default function BarComponent() {
                             <a href="https://www.linkedin.com/in/hecanyilmaz/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                         </li>
                         <li role="menu-item">
-                            <a href="/soon">Medium</a>
+                            <a>
+                                <Link to={"/soon"} className="mylink">Medium</Link>
+                            </a>
                         </li>
                     </ul>
                 </li>
                 <li role="menu-item" tabindex="0" aria-haspopup="true">
                     Photos
                     <ul role="menu">
-                        <li role="menu-item"><a href="/soon">Me</a></li>
-                        <li role="menu-item"><a href="/soon">Other</a></li>
+                        <li role="menu-item"><a><Link to={"/soon"} className="mylink">Me</Link></a></li>
+                        <li role="menu-item"><a ><Link to={"/soon"} className="mylink">Other</Link></a></li>
                     </ul>
                 </li>
             </ul>
