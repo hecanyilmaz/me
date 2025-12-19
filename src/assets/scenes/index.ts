3b48400e5b740308c1fafbc51011e45685e9df4d
hecanyilmaz <hcanylmzbotpre@gmail.com>
2025-12-19 17:23:02 +0300

    fix scenes page for chrome browser rendering

--- src/assets/scenes/index.ts
+++ src/assets/scenes/index.ts
@@ -1,12 +1,12 @@
 // Scene images
-import img_4 from './4.JPG';
-import img_3 from './3.JPG';
+import img_4 from './4.jpg';
+import img_3 from './3.jpg';
 import img_2 from './2.JPG';
-import img_1 from './1.JPG';
+import img_1 from './1.jpg';
 
 export const sceneImages: Record<string, string> = {
-  '4.JPG': img_4,
-  '3.JPG': img_3,
+  '4.jpg': img_4,
+  '3.jpg': img_3,
   '2.JPG': img_2,
-  '1.JPG': img_1,
+  '1.jpg': img_1,
 };
